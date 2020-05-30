814730c58da9bbd8fc39dd85915ea84796bb13d3
Sean <shan.tang@ergogroup.com>
2020-05-29 20:38:41 -0400

    need project

--- components/StyledText.js
+++ components/StyledText.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import { Text } from 'react-native';
+import * as React from "react";
+import { Text } from "react-native";
 
 export function MonoText(props) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
-=======
-import React from 'react';
-import { Text } from 'react-native';
-
-export class MonoText extends React.Component {
-  render() {
-    return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />;
-  }
->>>>>>> 35009e7cd7474097e614601b39f283b075535661
+  return (
+    <Text {...props} style={[props.style, { fontFamily: "space-mono" }]} />
+  );
 }
